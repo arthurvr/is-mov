0cd837d909665e312666947b0b84e081e394cf43
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 22:32:37 +0200

    ES2015ify tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,18 @@
-'use strict';
-var path = require('path');
-var test = require('ava');
-var readChunk = require('read-chunk');
-var isMov = require('../');
+import path from 'path';
+import test from 'ava';
+import readChunk from 'read-chunk';
+import isMov from '../';
 
 function check(fixture) {
 	return isMov(readChunk.sync(path.join(__dirname, 'fixture', fixture), 0, 8));
 }
 
-test('should detect mov files', function (t) {
+test('should detect mov files', t => {
 	t.true(check('fixture.mov'));
 	t.end();
 });
 
-test('should not give false positives', function (t) {
+test('should not give false positives', t => {
 	t.false(check('fixture.mp4'));
 	t.end();
 });
